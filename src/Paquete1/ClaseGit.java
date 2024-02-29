49f952dee1ac5a9d359fa333e52a63ff7d671e64
ferna <diegooavilez4912@gmail.com>
2024-02-29 09:54:02 -0600

    Primer cambio con git

---
+++ src/Paquete1/ClaseGit.java
@@ -0,0 +1,12 @@
+package Paquete1;
+
+public class ClaseGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Arriba los Pumas");
+		System.out.println("GIT Prueba");
+	}
+
+}
